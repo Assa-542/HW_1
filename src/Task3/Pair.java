73653cc4429ea2fe10df3b4422297a450f773cfe
Sergei <persona.four@yandex.ru>
2024-11-18 16:53:42 +0300

    addedd task3

---
+++ src/Task3/Pair.java
@@ -0,0 +1,27 @@
+package Task3;
+
+public class Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
